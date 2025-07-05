a7df0608e819c1cbe65e5d996e75e4d2cf23f028
Fransuelton <elton6103@gmail.com>
2025-07-05 04:19:25 -0300

    ♻️ refactor: improve docblock comments in Controller class for clarity

--- backend/app/Http/Controllers/Controller.php
+++ backend/app/Http/Controllers/Controller.php
@@ -4,5 +4,6 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    // This is a base controller that can be extended by other controllers.
+    // It can contain shared methods or properties for all controllers in the application.
 }
