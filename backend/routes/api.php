798cba34a718ec58a403c632fb4b49ba15239745
Fransuelton <elton6103@gmail.com>
2025-07-05 05:34:32 -0300

    🐛 fix: update delete route to use correct method name in UserController

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -8,4 +8,4 @@ Route::post('/user', [UserController::class, 'store']);
 Route::get('/user/{id}', [UserController::class, 'show']);
 Route::get('/users', [UserController::class, 'index']);
 Route::put('/user/{id}', [UserController::class, 'update']);
-Route::delete('/user/{id}', [UserController::class, 'delete']);
+Route::delete('/user/{id}', [UserController::class, 'destroy']);
