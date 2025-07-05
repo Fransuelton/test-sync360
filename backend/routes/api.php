8ff961cd1b324e8769fcf5c50c62d56e23ec755a
Fransuelton <elton6103@gmail.com>
2025-07-05 01:08:58 -0300

    ✨ feat: add update route for modifying user details

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -7,3 +7,4 @@ use App\Http\Controllers\UserController;
 Route::post('/user', [UserController::class, 'store']);
 Route::get('/user/{id}', [UserController::class, 'show']);
 Route::get('/users', [UserController::class, 'index']);
+Route::put('/user/{id}', [UserController::class, 'update']);
