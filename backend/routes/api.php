79464dfbd0c2840896730a00cfa7ec54c2005eed
Fransuelton <elton6103@gmail.com>
2025-07-05 01:01:00 -0300

    ✨ feat: add route for retrieving a user by ID

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,4 +5,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
 Route::post('/user', [UserController::class, 'store']);
+Route::get('/user/{id}', [UserController::class, 'show']);
 Route::get('/users', [UserController::class, 'index']);
