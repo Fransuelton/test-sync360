25fd296f315dbc2a9596a29753a89b17c8ac2dd1
Fransuelton <elton6103@gmail.com>
2025-07-07 08:33:58 -0300

    ✨ feat: add HelloWorld component with initial setup and click counter

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <section>
+    <h1>{{ msg }}</h1>
+    <button @click="count++">You clicked me {{ count }} times.</button>
+    
+  </section>
+</template>
+
+<style scoped>
+
+</style>
