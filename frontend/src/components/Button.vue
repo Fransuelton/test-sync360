bafa80034a4bffe8392615e3cfc10aca11a3cc98
Fransuelton <elton6103@gmail.com>
2025-07-07 20:27:35 -0300

    ✨ feat: add Button component

---
+++ frontend/src/components/Button.vue
@@ -0,0 +1,32 @@
+<script setup>
+defineProps({
+  title: String,
+});
+
+const emit = defineEmits(["click"]);
+</script>
+
+<template>
+  <button @click="emit('click')" type="button" class="button">
+    {{ title }}
+  </button>
+</template>
+
+<style scoped>
+.button {
+  background-color: var(--primary-color);
+  border-radius: 0.8rem;
+  cursor: pointer;
+  padding: 1.6rem;
+  color: var(--button-text-color);
+  font-weight: 500;
+  font-size: var(--default-font-size);
+  transition: background-color 0.2s;
+  border: none;
+  margin: 1rem 0;
+}
+
+.button:hover {
+  background-color: var(--primary-color-hover);
+}
+</style>
