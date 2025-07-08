3c18c7b490de3fb6129b5d04abe54c6ee3e21d35
Fransuelton <elton6103@gmail.com>
2025-07-08 08:05:50 -0300

    ✨ feat: create composables index for centralized exports

---
+++ frontend/src/composables/index.js
@@ -0,0 +1,8 @@
+/**
+ * 🔧 Composables Index - Barrel Export
+ * 
+ * Centraliza todas as exportações de composables.
+ */
+
+export { useToast } from './useToast.js'
+export { usePageTitle } from './usePageTitle.js'
