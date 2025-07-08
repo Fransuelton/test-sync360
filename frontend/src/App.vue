db13d62faa132a889132bbc0e373e8008c566e9c
Fransuelton <elton6103@gmail.com>
2025-07-08 08:15:18 -0300

    ✨ feat: implement toast notification system with container registration in App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,23 @@
-<script setup></script>
+<script setup>
+import { ref, onMounted } from "vue";
+import { ToastContainer } from "./components/index.js";
+import { useToast } from "./composables/index.js";
+
+const toastContainerRef = ref(null);
+const { registerContainer } = useToast();
+
+onMounted(() => {
+  if (toastContainerRef.value) {
+    registerContainer(toastContainerRef.value);
+  }
+});
+</script>
 
 <template>
-  <router-view />
+  <div id="app">
+    <router-view />
+    <ToastContainer ref="toastContainerRef" />
+  </div>
 </template>
 
 <style scoped></style>
